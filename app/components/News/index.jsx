4612a2cc341fe51b6d5ba99da089101c5d7c17d0
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-03-30 23:06:56 -0600

    Refactor the News component

--- app/components/News/index.jsx
+++ app/components/News/index.jsx
@@ -1,16 +1,27 @@
-import Preview from './Preview';
+import { Link } from '@remix-run/react';
 
 export default function News({ articles }) {
   return (
     <>
       <h1 className="font-bold text-2xl">Latest News</h1>
       <hr className="border-gray-400" />
-      {articles.map(
-        (article) => (
-          <Preview article={article} key={article.name} />
-        ),
-        [],
-      )}
+      {articles.map((article) => (
+        <Preview article={article} key={article.name} />
+      ))}
     </>
+  );
+}
+
+function Preview({ article }) {
+  const { title, date, description } = article;
+
+  return (
+    <article className="mb-6">
+      <div className="text-gray-400">{date}</div>
+      <Link to={`/news/${article.name}`} className="text-2xl">
+        {title}
+      </Link>
+      <p className="mt-2">{description}</p>
+    </article>
   );
 }
