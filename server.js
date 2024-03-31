e9a137edaf66a9f8db099e666a0a8b096a5e8d75
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-03-31 00:58:33 -0600

    Add compression, logging and caching directives for static assets

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import { createRequestHandler } from '@remix-run/express';
 import { installGlobals } from '@remix-run/node';
+import compression from 'compression';
 import express from 'express';
+import morgan from 'morgan';
 
 installGlobals();
 
@@ -13,31 +15,40 @@ const viteDevServer =
         }),
       );
 
+const remixHandler = createRequestHandler({
+  build: viteDevServer
+    ? () => viteDevServer.ssrLoadModule('virtual:remix/server-build')
+    : await import('./build/server/index.js'),
+});
+
 const app = express();
+
+app.use(compression());
+
+// http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
+app.disable('x-powered-by');
 
 // handle asset requests
 if (viteDevServer) {
   app.use(viteDevServer.middlewares);
 } else {
+  // Vite fingerprints its assets so we can cache forever.
   app.use(
     '/assets',
-    express.static('build/client/assets', {
-      immutable: true,
-      maxAge: '1y',
-    }),
+    express.static('build/client/assets', { immutable: true, maxAge: '1y' }),
   );
 }
+
+// Everything else (like favicon.ico) is cached for an hour. You may want to be
+// more aggressive with this caching.
 app.use(express.static('build/client', { maxAge: '1h' }));
 
-// handle SSR requests
-app.all(
-  '*',
-  createRequestHandler({
-    build: viteDevServer
-      ? () => viteDevServer.ssrLoadModule('virtual:remix/server-build')
-      : await import('./build/server/index.js'),
-  }),
-);
+app.use(morgan('tiny'));
 
-const port = 3000;
-app.listen(port, () => console.log(`http://localhost:${port}`));
+// handle SSR requests
+app.all('*', remixHandler);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () =>
+  console.log(`Express server listening at http://localhost:${port}`),
+);
