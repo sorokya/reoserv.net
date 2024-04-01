9f6570b56cfda050bac144f2dff9933d30c84b47
Richard Leek <richard@richardleek.com>
2024-04-01 21:17:06 +0200

    Use port 3030 by default

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
 // handle SSR requests
 app.all('*', remixHandler);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 app.listen(port, () =>
   console.log(`Express server listening at http://localhost:${port}`),
 );
