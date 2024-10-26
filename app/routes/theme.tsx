d2fa7f8c2b8b4ee1bec71d81a754b6170771711e
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-10-26 13:55:32 -0600

    Redirect on GET requests to the /theme route

--- app/routes/theme.tsx
+++ app/routes/theme.tsx
@@ -1,13 +1,21 @@
-import { type ActionFunctionArgs, redirect } from '@remix-run/node';
+import {
+  type ActionFunctionArgs,
+  type LoaderFunctionArgs,
+  redirect,
+} from '@remix-run/node';
 import { getThemeFromCookies, themeCookie } from '../.server/theme';
 
 export async function action({ request }: ActionFunctionArgs) {
   const currentTheme = await getThemeFromCookies(request);
   const newTheme = currentTheme === 'light' ? 'dark' : 'light';
 
-  return redirect(request.headers.get('Referer') ?? '/', {
+  throw redirect(request.headers.get('Referer') ?? '/', {
     headers: {
       'Set-Cookie': await themeCookie.serialize(newTheme),
     },
   });
+}
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  throw redirect(request.headers.get('Referer') ?? '/');
 }
