ccd5927d79f734f53392babaf7ea1bc591fc0f5f
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-10-26 14:32:02 -0600

    Clean unused imports

--- app/.server/get-news-article.ts
+++ app/.server/get-news-article.ts
@@ -1,10 +1,8 @@
-import { getClockOffset } from './utils/clock-offset';
 import { parseMarkdown } from './utils/parse-markdown';
 
 const NEWS_PATH = 'content/news';
 
 async function getNewsArticle(name: string, request: Request) {
-  const clockOffset = getClockOffset(request);
   const file = `${NEWS_PATH}/${name}.md`;
   return await parseMarkdown(file);
 }
