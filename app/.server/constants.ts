b5b2eabc6453d457314b2b4f6139a62fd38e0b5e
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-10-28 16:58:33 -0600

    Remove unneeded constants

--- app/.server/constants.ts
+++
@@ -1,7 +0,0 @@
-const GITHUB_RELEASE_URL =
-  'https://api.github.com/repos/sorokya/reoserv/releases/latest';
-
-const GITHUB_COMMITS_URL =
-  'https://github.com/sorokya/reoserv/commits/master.atom';
-
-export { GITHUB_COMMITS_URL, GITHUB_RELEASE_URL };
