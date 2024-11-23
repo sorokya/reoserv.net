33fc1fa0f9d74ffc04b48d8e4a7268d6dab8cfd8
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-11-22 23:13:44 -0700

    Get close to rr v7

--- vite.config.ts
+++ vite.config.ts
@@ -13,13 +13,13 @@ export default defineConfig({
   plugins: [
     remix({
       future: {
-        v3_singleFetch: true,
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_lazyRouteDiscovery: true,
         unstable_optimizeDeps: true,
-        unstable_routeConfig: true,
+        v3_fetcherPersist: true,
+        v3_lazyRouteDiscovery: true,
+        v3_relativeSplatPath: true,
+        v3_routeConfig: true,
+        v3_singleFetch: true,
+        v3_throwAbortReason: true,
       },
     }),
     imagetools({
