244aeae75f03ee4adf0a71a527c06e7c23f75a57
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-11-27 22:22:22 -0700

    Fix theme cookies

--- app/.server/theme.ts
+++ app/.server/theme.ts
@@ -3,9 +3,6 @@ import { createCookie } from 'react-router';
 const themeCookie = createCookie('theme', {
   httpOnly: true,
   secure: true,
-  sameSite: 'lax',
-  path: '/',
-  maxAge: 0,
 });
 
 async function getThemeFromCookies(request: Request) {
