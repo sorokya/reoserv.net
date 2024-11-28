ef6ebb8f4df2a5683eb84396a0257dc25792fb9d
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-11-27 22:29:43 -0700

    Fix table of contents

--- app/routes/news.$name/table-of-contents.tsx
+++ app/routes/news.$name/table-of-contents.tsx
@@ -17,13 +17,13 @@ function TableOfContents({ toc }: TableOfContentsProps) {
   }
 
   return (
-    <nav className="sticky top-8 w-full border border-amber-6 border-dashed bg-amber-2 p-6 opacity-60 transition-opacity duration-200 hover:border-amber-9 hover:opacity-100">
+    <nav className="sticky top-8 w-full border border-amber-6 border-dashed bg-amber-2 p-4 opacity-80 transition-all duration-100 hover:border-amber-9 hover:opacity-100">
       <ul className="space-y-2">
         {filteredToc.map((item) => (
           <li key={item.id} className={`${item.level === 3 ? 'ml-4' : ''}`}>
             <a
               href={`#${item.id}`}
-              className="line-clamp-1 text-amber-11 text-sm transition-colors duration-200 hover:font-semibold hover:text-amber-12"
+              className="line-clamp-1 text-amber-11 text-sm transition-colors duration-100 hover:font-semibold"
             >
               {item.raw}
             </a>
