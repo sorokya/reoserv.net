5239c8f63b78f58976f316fed55ec2e581c1e1e2
Richard Leek <richard@richardleek.com>
2026-03-04 08:13:29 +0100

    Add android death banner

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,8 +3,8 @@ import type { Config } from '@react-router/dev/config';
 export default {
   future: {
     unstable_optimizeDeps: true,
-    unstable_splitRouteModules: true,
-    unstable_viteEnvironmentApi: true,
+    v8_splitRouteModules: true,
+    v8_viteEnvironmentApi: true,
   },
   ssr: true,
 } satisfies Config;
